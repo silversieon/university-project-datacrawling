0969ac1f68b7b69e329029c012934330c8795dfa
Ksieon <keumsiun0503@gmail.com>
2025-11-27 22:34:46 +0900

    :sparkles: Feat: test fastapi

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from API.age_voice_phising.age_vp import age_voice_phishing
 from API.cyber_crime_deep_analysis.cyber_crime_deep_a1 import property_crime
 from API.cyber_crime_deep_analysis.cyber_crime_deep_a2 import fraud_damage
 from API.cyber_crime_deep_analysis.cyber_crime_deep_a3 import online_activity
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 def main():
     # regional_voice_phishing()
