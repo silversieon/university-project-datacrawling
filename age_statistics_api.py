4759f7b2bc8646d59fa220e3734c17e49cb0678c
Comeoksal <keumsiun0503@gmail.com>
2025-11-28 01:50:56 +0900

    :recycle: Refactor: add cluster analysis

--- age_statistics_api.py
+++ age_statistics_api.py
@@ -1,11 +1,8 @@
 from fastapi import APIRouter
 from API.age_voice_phising.age_vp import request_age_api
+from sklearn.cluster import KMeans
+import numpy as np
 router = APIRouter()
-
-def evaluate(pre_grow_rate, mean_grow_rate):
-    # (이전 년도 대비 증가율이 평균 대비 증가율 보다 중요한 지표라고 생각하여 0.7이라는 가중치를 주었습니다!)
-    hazardScore = pre_grow_rate * 0.7 + mean_grow_rate * 0.3
-    return hazardScore, True if hazardScore >= 20 else False
 
 @router.get("")
 def get_age_hazard_statistics():
@@ -24,15 +21,36 @@ def get_age_hazard_statistics():
     mean = data.drop(latest_year).mean()
     mean_grow_rate = (data.loc[latest_year] - mean)/mean * 100
 
+    X = np.column_stack([
+        pre_grow_rate,
+        mean_grow_rate
+    ])
+
+    # 3개의 군집으로 분류하여 군집분석 (저위험, 중간위험, 고위험)
+    kmeans = KMeans(n_clusters=3, random_state=0, n_init='auto')
+
+    # 각 연령대를 3개의 군집으로 나누기
+    labels = kmeans.fit_predict(X)
+
+    # (이전 년도 대비 증가율, 평균 대비 증가율) 세 군집의 중앙을 평가
+    centers = kmeans.cluster_centers_
+    cluster_score = np.argsort(centers.mean(axis=1))
+
+    # 중앙 위치가 작은(두 증가율 평균이 가장 낮은) 군집부터 위험도 배정
+    hazard_score = {
+        cluster_score[0]: "LOW",
+        cluster_score[1]: "MID",
+        cluster_score[2]: "HIGH"
+    }
+
     # 해당 연령대의 올해 보이스피싱 위험성 평가 api 응답 생성
     response = {}
-    for age in age_columns:
-        score, hazard = evaluate(pre_grow_rate[age], mean_grow_rate[age])
+    for i, age in enumerate(age_columns):
         response[age] = {
             "growth_rate_compared_pre_year": round(pre_grow_rate[age], 2),
             "growth_rate_compared_mean_year": round(mean_grow_rate[age], 2),
-            "hazardScore": round(score, 2),
-            "isHazard": hazard
+            "cluster": int(labels[i]),
+            "isHazard": hazard_score[labels[i]]
         }
 
     return response
--- regional_statistics_api.py
+++ regional_statistics_api.py
@@ -1,12 +1,9 @@
 from fastapi import APIRouter
 from API.regional_voice_phishing.regional_vp import request_regional_api
+from sklearn.cluster import KMeans
+import numpy as np
 
 router = APIRouter()
-
-def evaluate(pre_grow_rate, mean_grow_rate):
-    # (이전 년도 대비 증가율이 평균 대비 증가율 보다 중요한 지표라고 생각하여 0.7이라는 가중치를 주었습니다!)
-    hazardScore = pre_grow_rate * 0.7 + mean_grow_rate * 0.3
-    return hazardScore, True if hazardScore >= 20 else False
 
 @router.get("")
 def get_regional_risk_statistics():
@@ -25,15 +22,38 @@ def get_regional_risk_statistics():
     mean = data.drop(latest_year).mean()
     mean_grow_rate = (data.loc[latest_year] - mean)/mean * 100
 
+    X = np.column_stack([
+        pre_grow_rate,
+        mean_grow_rate
+    ])
+
+    # 5개의 군집으로 분류하여 군집분석 (초저위험, 저위험, 중간위험, 고위험, 초고위험)
+    kmeans = KMeans(n_clusters=5, random_state=0, n_init='auto')
+
+    # 각 지역을 5개의 군집으로 나누기
+    labels = kmeans.fit_predict(X)
+
+    # (이전 년도 대비 증가율, 평균 대비 증가율) 다섯 군집의 중앙을 평가
+    centers = kmeans.cluster_centers_
+    cluster_score = np.argsort(centers.mean(axis=1))
+
+    # 중앙 위치가 작은(두 증가율 평균이 가장 낮은) 군집부터 위험도 배정
+    hazard_score = {
+        cluster_score[0]: "VERY_LOW",
+        cluster_score[1]: "LOW",
+        cluster_score[2]: "MID",
+        cluster_score[3]: "HIGH",
+        cluster_score[4]: "VERY_HIGH"
+    }
+
     # 해당 지역의 올해 보이스피싱 위험성 평가 api 응답 생성
     response = {}
-    for region in region_columns:
-        score, hazard = evaluate(pre_grow_rate[region], mean_grow_rate[region])
+    for i, region in enumerate(region_columns):
         response[region] = {
             "growth_rate_compared_pre_year": round(pre_grow_rate[region], 2),
             "growth_rate_compared_mean_year": round(mean_grow_rate[region], 2),
-            "hazardScore": round(score, 2),
-            "isHazard": hazard
+            "cluster": int(labels[i]),
+            "isHazard": hazard_score[labels[i]]
         }
     
     return response
